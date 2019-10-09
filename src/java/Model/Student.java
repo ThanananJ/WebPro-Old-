8fb7a41a59038440f7105e7071e6839d589d5d9e
Mark <o.onepiece9@gmail.com>
2019-10-09 13:43:08 +0700

    [UPDATE] Get/Set Student

--- src/java/Model/Student.java
+++ src/java/Model/Student.java
@@ -11,4 +11,52 @@ package Model;
  */
 public class Student {
     
+    private String userName;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String level;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+    
+    
+    
 }
