e60896d23d7fcf8e1ea155b792ef85eaeca80f01
Nattawanee <nattawanee.sks@mail.kmutt.ac.th>
2019-10-10 15:00:26 +0700

    [Update]PackageModelName

--- src/java/quinn/model/Question.java
+++ src/java/quinn/model/Question.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model;
+package quinn.model;
 
 /**
  *
  * @author Mark
  */
-public class Exam {
+public class Question {
     
 }
