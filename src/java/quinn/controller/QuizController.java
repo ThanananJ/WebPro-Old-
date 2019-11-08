8eda7137a3b2f54c01a152bf831850b8f7fd0b64
Nattawanee <nattawanee.sks@mail.kmutt.ac.th>
2019-11-08 17:16:56 +0700

    [ADD] Quiz Controller

---
+++ src/java/quinn/controller/QuizController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quinn.controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import quinn.db.BuildConnection;
+import quinn.model.Quiz;
+import quinn.model.Student;
+import quinn.model.Teacher;
+
+/**
+ *
+ * @author nattawanee.sks
+ */
+public class QuizController {
+    
+    public static List<Quiz> findByDesc(String find){
+        List<Quiz> list = null;
+        Quiz q;
+        Connection conn = BuildConnection.getConnection();
+        try {
+            PreparedStatement pstm = conn.prepareStatement("select * from quizes where description LIKE ?");
+            pstm.setString(1, "%"+find+"%");
+            ResultSet rs = null;
+            rs = pstm.executeQuery();
+            while(rs.next()){
+                if(list == null){
+                    list = new ArrayList(100);
+                }
+                q = new Quiz(rs.getString("quiz_id"), rs.getString("description"), rs.getString("subject"), rs.getString("q_type"), rs.getString("t_id"), rs.getString("class_id"));
+                list.add(q);
+            }
+            rs.close();
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Student.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+    
+    public static void main(String[] args) {
+        QuizController qc = new QuizController();
+        List<Quiz> q = qc.findByDesc("Eng");
+        System.out.println(q.get(0).getDescription());
+    }
+}
---
+++ src/java/quinn/model/Quiz.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quinn.model;
+
+import quinn.controller.TeacherController;
+
+/**
+ *
+ * @author Mark
+ */
+public class Quiz {
+    private String quiz_id;
+    private String description;
+    private String subject;
+    private String type;
+    private String quizOwner;
+    private String classAllowToDo;
+
+    public Quiz() {
+    }
+
+    public Quiz(String quiz_id, String description, String subject, String type, String quizOwner, String classAllowToDo) {
+        this.quiz_id = quiz_id;
+        this.description = description;
+        this.subject = subject;
+        this.type = type;
+        this.quizOwner = quizOwner;
+        this.classAllowToDo = classAllowToDo;
+    }
+
+    
+    public String getQuiz_id() {
+        return quiz_id;
+    }
+
+    public void setQuiz_id(String quiz_id) {
+        this.quiz_id = quiz_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        if(type.equals("1")){
+            this.type = "FillAnswer";
+        } else {
+            this.type = "MultipleChoice";
+        }
+    }
+
+    public String getQuizOwner() {
+        return quizOwner;
+    }
+
+    public void setQuizOwner(String quizOwner) {
+        TeacherController tc = new TeacherController();
+        Teacher owner = tc.findByTeacherId(quizOwner);
+        this.quizOwner = owner.getUserName();
+    }
+
+    public String getClassAllowToDo() {
+        return classAllowToDo;
+    }
+
+    public void setClassAllowToDo(String classAllowToDo) {
+        this.classAllowToDo = classAllowToDo;
+    }
+    
+    
+}
